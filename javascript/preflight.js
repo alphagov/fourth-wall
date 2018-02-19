a211349f049a10069de11a9077c6f0dc3b7782ca
Toby Lorne <toby.lornewelch-richards@digital.cabinet-office.gov.uk>
2018-02-19 16:12:14 +0000

    BAU: Correct isUnsafe logic
    Logic is hard
    
    solo @tlwr

--- javascript/preflight.js
+++ javascript/preflight.js
@@ -1,7 +1,7 @@
 function runPreFlightChecks() {
   const isHttp      = window.location.protocol == 'http:',
         isLocalhost = window.location.hostname == 'localhost'
-        isUnsafe    = !isHttp || !(isHttp && isLocalhost);
+        isUnsafe    = isHttp && !isLocalhost;
 
   if (isUnsafe) {
     const isHttpMessage = [
