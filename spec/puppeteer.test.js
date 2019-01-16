3fea5f76278fccdc3903fc3835d7fc8e950baff0
Alistair Laing <alistair.laing@digital.cabinet-office.gov.uk>
2019-01-16 16:07:06 +0000

    Add Jest in conjunction with puppeteer
    Use Jest as the assestion library when running puppeteer. We might want
    to convert from Jasmine to Jest but thats a bigger piece of work

--- spec/puppeteer.test.js
+++ spec/puppeteer.test.js
@@ -1,13 +1,28 @@
-const puppeteer = require('puppeteer');
+/* eslint-env jest */
 
-(async () => {
-  const browser = await puppeteer.launch()
-  const page = await browser.newPage()
-  // TODO: make this a generic url to work locally or in Travis
-  await page.goto('file:///Users/alistairlaing/work/fourth-wall/spec/index.html')
-  await page.screenshot({path: 'example.png'})
-  await page.waitForSelector('.alert')
-  await page.evaluate(() => { return document.body.querySelector('passingAlert')})
-  await page.screenshot({path: 'example1.png'})
+const puppeteer = require('puppeteer')
+let browser
+let page
+
+beforeEach( async (done) => {
+  browser = await puppeteer.launch({headless: false})
+  page = await browser.newPage()
+  done()
+})
+
+afterEach(async (done) => {
+  await page.close();
   await browser.close();
-})()
+  done();
+})
+
+it('passes all tests', async (done) => {
+  // TODO: make this a generic url to work locally or in Travis
+  // await page.goto('file:///Users/alistairlaing/work/fourth-wall/spec/index.html')
+  await page.goto('http://localhost:8000/spec/index.html')
+  await page.waitForSelector('.passingAlert')
+  const passed = await page.evaluate( () => document.querySelector('.passingAlert').textContent )
+
+  expect(passed).toEqual('Passing 54 specs')
+  done()
+})
